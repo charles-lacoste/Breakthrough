2cc06591616258a055f1b2bf5fa0b0453ed31b1a
William <william_huynh22@hotmail.com>
2017-04-08 00:58:01 -0400

    Revert "Scripts"
    This reverts commit 01ab5ae3f540f0b756e05a67e8dbde390b909f51.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,89 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    [SerializeField]
-    private GameObject bullet;
-
-    private Transform _gunpos;
-    private Rigidbody _rb;
-    private Animator _anim;
-    private CharacterController _cc;
-    private Camera _cam;
-
-    private float _speed;
+public class PlayerController : MonoBehaviour {
+    Animator _anim;
+    Rigidbody _rb;
+    float _speed;
 
     // Use this for initialization
-    private void Start()
-    {
+    void Start() {
         _anim = GetComponent<Animator>();
         _rb = GetComponent<Rigidbody>();
-        _cc = GetComponent<CharacterController>();
-        _cam = GameObject.Find("Camera").GetComponent<Camera>();
-        _gunpos = GameObject.Find("GunPoint").transform;
         _speed = 4.0f;
     }
 
     // Update is called once per frame
-    private void Update()
-    {
-        Aim();
+    void Update() {
+        //Aim();
         Move();
-        Shoot();
     }
 
-    private void Move()
-    {
+    void Move() {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizontal, 0, vertical);
-
-        if (!Mathf.Approximately(vertical, 0.0f) || !Mathf.Approximately(horizontal, 0.0f))
-        {
-            //if (_cc.isGrounded)
-            {
-                movement = transform.TransformDirection(movement);
-                movement *= _speed;
-                //transform.Translate(movement * _speed * Time.deltaTime, Space.Self);
-                _anim.SetBool("Running", true);
-                //if (!_footsteps.isPlaying)
-                //    _footsteps.Play();
-            }
-        }
-        else
-        {
-            //_rb.velocity = Vector3.zero;
+        if (!Mathf.Approximately(vertical, 0.0f) || !Mathf.Approximately(horizontal, 0.0f)) {
+            _rb.velocity = new Vector3(horizontal, 0, vertical) * _speed;
+            _anim.SetBool("Running", true);
+            //if (!_footsteps.isPlaying)
+            //    _footsteps.Play();
+        } else {
+            _rb.velocity = Vector3.zero;
             _anim.SetBool("Running", false);
         }
-        _cc.Move(movement * Time.deltaTime);
     }
 
-    private void Aim()
-    {
-        transform.forward = _cam.transform.forward;
-        transform.rotation = Quaternion.Euler(0.0f, transform.rotation.eulerAngles.y, 0.0f);
-
-        /*
+    void Aim() {
         Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.z, 0);
         Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
         lookPos = lookPos - transform.position;
         float angle = Mathf.Atan2(lookPos.z, lookPos.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.down); // Turns Right
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.up); //Turns Left
-        */
-    }
-
-    private void Shoot()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            //Instantiate(bullet, transform.forward, Quaternion.identity);
-            //GameObject c = (GameObject)Instantiate(bullet, _gunpos.transform.position, _gunpos.transform.rotation);
-            //GameObject c = (GameObject)Instantiate(bullet, _cam.transform.forward, _cam.transform.rotation);
-        }
-        else
-        {
-        }
     }
 }
